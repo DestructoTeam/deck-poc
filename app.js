5b358aabffeea27596be30fc421ad1858c119c4f
Mudada <mael.nicolas77@gmail.com>
2018-10-07 15:17:22 +0200

    add home page

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const express = require("express")
+const express = require("express");
+const pug = require("pug");
 
+let app = express();
+app.set("view engine", "pug");
 
+app.get("/", (req, res) => {
+    res.render('app');
+});
+
+app.listen("8000");
