135ed1fe8bb63326765a2ca44d0b1978129094eb
Mudada <mael.nicolas77@gmail.com>
2018-10-07 18:21:25 +0200

    did the front, need to apply a better css

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const pug = require("pug");
-
+const listing = require("./src/listing/route");
 
 let app = express();
 app.set("view engine", "pug");
+app.use(express.static(__dirname + "/includes/css"));
 
+app.use("/list", listing);
 app.get("/", (req, res) => {
-    res.render('app');
+    let lists = [{id: 1, name: "Liste1"}, {id: 2, name: "Liste2"}, {id: 3, name: "Liste3"}];
+    res.render(
+        'app',
+        {lists: lists}
+    );
 });
 
 app.listen("8000");
--- src/listing/route.js
+++ src/listing/route.js
@@ -1,3 +1,16 @@
 const express = require("express");
 
-let app = express();
+let app = module.exports = express();
+
+app.get("/:id", (req, res) => {
+    const list = {
+        id: req.params.id,
+        name: "Liste",
+        cards: [{name: "Squee"}]
+    };
+
+    res.render(
+        'listing/list',
+        {list: list}
+    );
+});
