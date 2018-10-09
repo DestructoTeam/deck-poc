aec42196d0faf000019fac79695fe734203bc8bc
MoreauA <moreau_a@etna-alternance.net>
2018-10-09 10:07:08 +0200

    Advanced CSS

--- app.js
+++ app.js
@@ -2,17 +2,39 @@ const express = require("express");
 const pug = require("pug");
 const listing = require("./src/listing/route");
 
-let app = express();
+const app = express();
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/includes/css"));
 
 app.use("/list", listing);
 app.get("/", (req, res) => {
-    let lists = [{id: 1, name: "Liste1"}, {id: 2, name: "Liste2"}, {id: 3, name: "Liste3"}];
+    const lists = [{id: 1, name: "List 1"}, {id: 2, name: "List 2"}, {id: 3, name: "List 3"}];
     res.render(
         'app',
         {lists: lists}
     );
 });
+app.get("/profile", (req, res) => {
+    const profile = [{id: 1, name: "John Doe", email: "john.doe@gmail.com"}];
+    res.render(
+        'profile',
+        {profile: profile}
+    );
+});
+app.get("/matches", (req, res) => {
+    const matches = [{id: 1, name: "Johnny Doe"}, {id: 2, name: "Jane Doe"}];
+    res.render(
+        'matches',
+        {matches: matches}
+    );
+});
+app.get("/match", (req, res) => {
+    const match = {id: 1, name: "Johnny Doe", cards: [{id: 1, name: "Communion avec la lave", imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=395577&type=card"}], messages: [{id: 1, who: "You", message: "Hi there !"}, {id: 2, who: "Johnny D.", message: "Hi."}]};
+    res.render(
+        'match',
+        {match: match}
+    );
+});
+
 
 app.listen("8000");
--- src/listing/route.js
+++ src/listing/route.js
@@ -1,12 +1,18 @@
 const express = require("express");
 
-let app = module.exports = express();
+const app = module.exports = express();
 
 app.get("/:id", (req, res) => {
     const list = {
         id: req.params.id,
-        name: "Liste",
-        cards: [{name: "Squee"}]
+        name: "List 1",
+        cards: [{
+          name: "Esclavagiste de l'effroi",
+          imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=295556&type=card"
+       },{
+          name: "Communion avec la lave",
+          imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=395577&type=card"
+       }]
     };
 
     res.render(
