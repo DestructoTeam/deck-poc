9ad6d6e2c63643f7ec0c020ec6772836c09ee9d3
Mudada <mael.nicolas77@gmail.com>
2018-10-31 16:20:38 +0100

    clean some console.log

--- includes/js/search-card.js
+++ includes/js/search-card.js
@@ -1,10 +1,7 @@
 function searchCard(name) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        console.log(this.status);
-        console.log(this.readyState);
         if (this.readyState == 4 && this.status == 200) {
-            console.log("You're in");
             console.log(this.response);
         }
     };
