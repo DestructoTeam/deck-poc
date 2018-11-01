64ed72c2a26af41b78735b4cff2242fbf3da9dd6
moreaua <moreaua@gitlab.stordata.fr>
2018-11-01 14:48:20 +0100

    Need ccs approvement

--- includes/js/search-card.js
+++ includes/js/search-card.js
@@ -2,7 +2,24 @@ function searchCard(name) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.response);
+            document.getElementById("ourContainer").innerHTML = "";
+            const obj = JSON.parse(this.response);
+            const documentFragment = document.createDocumentFragment();
+            obj.cards.forEach(function(element) {
+                if (typeof element.url !== "undefined") {
+                    var cardFragment = document.createElement('div');
+                    var img = document.createElement('IMG');
+                    var but = document.createElement('button');
+                    cardFragment.appendChild(document.createElement('p')).innerHTML = element.name;
+                    img.setAttribute('src', element.url);
+                    but.setAttribute('class', "btn btn-primary");
+                    but.setAttribute('style', "font-size:2rem;");
+                    cardFragment.appendChild(img);
+                    cardFragment.appendChild(but).innerHTML = "Add";
+                    documentFragment.appendChild(cardFragment);
+                }
+            });
+            document.getElementById("ourContainer").appendChild(documentFragment);
         }
     };
     xhttp.open("GET", "/cards/search/" + name, true);
