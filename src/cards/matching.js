d14805ca74a48668547e37c39a7ffb3369651951
Mudada <mael.nicolas77@gmail.com>
2018-11-02 02:22:37 +0100

    added function to compare a list and a [user]

--- src/cards/matching.js
+++ src/cards/matching.js
@@ -1,3 +1,6 @@
+const bdd = require("../../bdd/bdd_query");
+const R = require('ramda');
+
 const flip = f => a => b => f(b)(a);
 
 const contains = val => xs => xs.indexOf(val) !== -1;
@@ -7,6 +10,19 @@ const any = fn => xs => xs.some(fn);
 const compareLists =
       list1 =>
       list2 => {
-          if (any(flip(contains)(list2))(list1)) return list2;
+          if (list1.wanted === list2.wanted)
+              return {};
+          if (any(flip(contains)(list2.card_list))(list1.card_list)) return list2;
           return {};
       };
+
+const getId = user => user.id;
+
+const matchUser = (list, users) => {
+    const users_id = R.map(getId, users);
+    const users_list = R.map(bdd.getlist, users_id);
+    return Promise.all(users_list).then(xs => {
+        const lists = xs[0];
+        return R.map(compareLists(list), lists);
+    });
+};
